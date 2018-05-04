306ae8cfde6eb32f19402afd28106ac9038bea43
svichas <stefanos@vichas.org>
2018-05-05 00:25:10 +0300

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,14 @@ var pug = require('gulp-pug');
 
 
 gulp.task('pug-compile', function() {
-
-	return gulp.src('views/*.pug')
+	return gulp.src(['views/*.pug'])
 	.pipe(pug({
 
 	}))
 	.pipe(gulp.dest(""));
-
 });
 
 
 gulp.task("pug-watch", function() {
-
-	return gulp.watch('views/*.pug', ["pug-compile"]);
-
+	return gulp.watch(['views/*.pug']);
 });
