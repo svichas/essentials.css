4b1dbe2a809d583beb666cbf8db318eeed1ff2aa
svichas <stefanos@vichas.org>
2018-06-23 13:20:02 +0300

    Added css autoprefixer in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp   = require("gulp");
 const sass   = require("gulp-sass");
 const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
 
 //creating compile task
 gulp.task("compile-sass", function() {
@@ -10,12 +11,20 @@ gulp.task("compile-sass", function() {
 	//compile sass compressed version
 	gulp.src("scss/*.scss")
 	.pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
+	.pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
 	.pipe(rename("essentials.min.css"))
 	.pipe(gulp.dest("dist/"));
 
 	//compile sass uncompressed version
 	gulp.src("scss/*.scss")
 	.pipe(sass().on("error", sass.logError))
+	.pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
 	.pipe(rename("essentials.css"))
 	.pipe(gulp.dest("dist/"));
 
@@ -27,6 +36,5 @@ gulp.task("watch-sass", function() {
 	//watching sass files on src directory
 	gulp.watch("scss/*.scss", ["compile-sass"]);
 	gulp.watch("scss/**/*.scss", ["compile-sass"]);
-
 
 });
